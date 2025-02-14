5c66640ef1227fdae228d4a47c7b252e24edc866
pluto50015 <87066856+pluto50015@users.noreply.github.com>
2025-02-15 03:54:50 +0900

    Updated port settings

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ if __name__ == '__main__':
 
     #port = int(os.environ.get("PORT", 10000))
     #app.run(host="0.0.0.0", port=port)0
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
